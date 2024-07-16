b41dc050c33683da27ffe008ac4dd1a4baa4055b
KEH-ERNI <khyla.hondrade@betterask.erni>
2024-07-16 15:08:04 +0800

    Add mobile responsiveness for Index view.

---
+++ HondradeCRUD/Controllers/BootcampersController.cs
@@ -0,0 +1,23 @@
+﻿using HondradeCRUD.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HondradeCRUD.Controllers
+{
+    public class BootcampersController : Controller
+    {
+
+        private readonly DBContext context;
+
+        public BootcampersController(DBContext context)
+        {
+            this.context = context; 
+        }
+
+
+        public IActionResult Index()
+        {
+            var bootcampers = context.Bootcampers.ToList();
+            return View(bootcampers);
+        }
+    }
+}
